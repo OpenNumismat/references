d44602bc786b42f5c8f8a645bc10e410cb732358
ignatov <ignatov_vi@nartis.ru>
2024-01-25 11:24:44 +0300

    Add regions from World Factbook

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Sources:
 - data/icons/flags/famfamfam - https://github.com/legacy-icons/famfamfam-flags/tree/1.0.0
 - data/icons/flags/GoSquared - https://github.com/gosquared/flags
-- data/icons/flags/Flagpack - 
-- data/icons/flags/StefanGabos
+- data/icons/flags/StefanGabos - https://github.com/stefangabos/world_countries/tree/v2.8.2
+
 - src/ISO-3166-Countries-with-Regional-Codes.json - https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/tree/v9.0
+- src/The_World_Factbook.csv - https://www.cia.gov/the-world-factbook/field/area/country-comparison/
---
+++ parsers/The_World_Factbook.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import json
+
+def str2latin(string):
+    return string.replace('ô', 'o').replace('ó', 'o').replace('ç', 'c').replace('é', 'e').replace('ë', 'e').replace('ā', 'a').replace('ã', 'a').replace('í', 'i').replace('ê', 'e').replace('ş', 's').replace('Đ', 'D').replace('ồ', 'o').replace('à', 'a')
+
+def country2countrydata(country, orig_data):
+    for region in orig_data["regions"]:
+        for orig_country in region["countries"]:
+            if str2latin(orig_country["name"]) == country:
+                return orig_country
+    return None
+
+
+subregions_order = [
+'Central Asia',
+'Antarctica',
+'North America',
+'East and Southeast Asia',
+'South America',
+'Australia and Oceania',
+'South Asia',
+'Africa',
+'Middle East',
+'Europe',
+'Central America and the Caribbean',
+]
+
+subregions = []
+subregions_data = {}
+with open('../src/The_World_Factbook.csv', encoding='utf-8') as subregions_file:
+    spamreader = csv.reader(subregions_file)
+    next(spamreader, None)
+    for row in spamreader:
+        subregions_data[row[0]] = row[5]
+
+with open('../data/countries.json', encoding='utf-8') as orig_file:
+    orig_data = json.load(orig_file)
+
+result = {
+    "regions": []
+}
+
+for region in subregions_order:
+    result["regions"].append({
+        "name": region,
+        "countries": [],
+    })
+
+for country, region in subregions_data.items():
+    for r in result["regions"]:
+        if r["name"] == region:
+            orig_country_data = country2countrydata(country, orig_data)
+            if orig_country_data:
+                r["countries"].append(orig_country_data)
+            else:
+                print("Missed data for", country)
+
+with open('../data/Regions (World Factbook).json', 'w', encoding='utf8') as json_file:
+    json.dump(result, json_file, ensure_ascii=False, indent=2)
