67b0520df861f9086c12115ac3b9afaa5329cce6
ignatov <ignatov_vi@nartis.ru>
2024-01-29 16:50:30 +0300

    dependend -> dependent

--- parsers/add_alpha3.py
+++ parsers/add_alpha3.py
@@ -12,7 +12,7 @@ with open('../data/countries_old.json', encoding='utf-8') as file:
 with open('../data/unrecognized_countries.json', encoding='utf-8') as file:
     unrecognized_data = json.load(file)
 
-with open('../data/dependend_countries.json', encoding='utf-8') as file:
+with open('../data/dependent_countries.json', encoding='utf-8') as file:
     dependend_data = json.load(file)
 
 for region in orig_data['regions']:
@@ -32,7 +32,7 @@ for region in orig_data['regions']:
 
         if 'alpha3' in country and country['alpha3'] in dependend_data:
             if 'unrecognized' not in country:
-                country['dependend'] = True
+                country['dependent'] = True
 
 
 with open('../data/countries.json', 'w', encoding='utf8') as json_file:
