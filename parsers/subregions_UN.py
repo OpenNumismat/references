aa8fac4708d8997951fa2960b260e9909be9c863
ignatov <ignatov_vi@nartis.ru>
2024-01-31 13:29:52 +0300

    Use alternative country name as default

--- parsers/subregions_UN.py
+++ parsers/subregions_UN.py
@@ -1,10 +1,22 @@
 import csv
 import json
 
+
+def get_alternative_names(country):
+    country = country.lower()
+    for names in alternative_names.values():
+        for name in names:
+            if name.lower() == country:
+                return names
+    return [country,]
+
 def country2countrydata(country, orig_data):
+    names = get_alternative_names(country)
     for orig_country in orig_data["countries"]:
-        if orig_country["name"] == country:
-            return orig_country
+        key = orig_country["name"].lower()
+        for name in names:
+            if name.lower() == key:
+                return orig_country
     return None
     
 
@@ -17,6 +29,9 @@ with open('../src/UNSD — Methodology.csv', encoding='utf-8') as file:
         if not region:
             region = row[5]
         countries_list[row[8]] = region
+
+with open('../data/countries_alternative_names.json', encoding='utf-8') as file:
+    alternative_names = json.load(file)
 
 with open('../data/country_currency.json', encoding='utf-8') as orig_file:
     orig_data = json.load(orig_file)
